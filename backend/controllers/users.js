b7c9f0fd864a157105482649930b0bf20110cd18
daryamaximenko <i@dkarushina.ru>
2023-04-26 22:23:33 +0200

    fix

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -130,6 +130,7 @@ module.exports.login = (req, res, next) => {
         httpOnly: true,
         sameSite: 'none',
         secure: true,
+        domain: backend.nomoredomains.monster,
       }).send({ message: messages.ok });
     })
     .catch(next);
