605bcb2acac3ef6c34c5555d466956631299d06d
daryamaximenko <i@dkarushina.ru>
2023-04-26 20:56:30 +0200

    fix

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -128,7 +128,7 @@ module.exports.login = (req, res, next) => {
       res.cookie('jwt', token, {
         maxAge: 36000000,
         httpOnly: true,
-        sameSite: 'none',
+        domain: 'nomoredomains.monster',
         secure: true,
       }).send({ message: messages.ok });
     })
