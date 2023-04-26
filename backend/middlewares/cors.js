5620c755bc2cdb6560fdaf35e481f1c318335c91
daryamaximenko <i@dkarushina.ru>
2023-04-26 20:33:44 +0200

    fix

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,21 +1,18 @@
 // Массив доменов, с которых разрешены кросс-доменные запросы
 const allowedCors = [
-  'http://domainname.praktikum.nomoredomains.monster',
   'https://domainname.praktikum.nomoredomains.monster',
 ];
 
-// Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
-const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-
 module.exports = (req, res, next) => {
+  res.header('Access-Control-Allow-Credentials', true);
+  
   const { origin } = req.headers;
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
   }
 
   if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
     res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
     return res.end();
   }
